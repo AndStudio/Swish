54a088cae5e2669281349f5e29706e9cc30eeb8b
clayjmills <AnimalHouse@Clays-MacBook-Pro.local>
2017-04-11 11:10:36 -0600

    add comments to Usercontroller for what to add

--- Swish/UserController.swift
+++ Swish/UserController.swift
@@ -10,4 +10,18 @@ import Foundation
 
 class UserController {
     
+    //property for the access token
+    
+    // func that takes completion handler
+    
+    // create url string using the access token and append the accessToken at the end
+    
+    // NSURLSession to connect the endpoint
+    
+    // NSURL to create url using the string 
+    
+    // create a task and put in the url. use an if else statement
+    
+    //implement the model.swift file information inside the else if before you return
+    
 }
