ed69536d143087336559881a26c28f67e61e9967
clayjmills <claymills5@gmail.com>
2017-04-13 16:48:49 -0600

    updated model, model controller, view controller, can run

--- Swish/UserController.swift
+++ Swish/UserController.swift
@@ -6,37 +6,46 @@
 //  Copyright © 2017 And. All rights reserved.
 //
 
-// use url with queries
-// https://api.dribbble.com/v1/users/1/shots?
 // access_token=a1590f48ee53ae2d172f3c49a444ce3d658e92cf7c95a91cc39eebbd4c5197cd
 
 
-// create baseURL, create func with @escaping completion, cal the NetworkController, unwrap the base url, append the url, unwrap the data and error, turn data into jsonDictionary, call the images and unwrap
 
 
 import Foundation
 
-//class UserController {
-//    
-//    static let baseURL = URL(string: "https://api.dribbble.com/v1/users/1/shots?")
-//    
-//    static func fetchUser(userID: Int, completion: @escaping(User?) -> Void) {
-//        guard let unwrappedURL = baseURL else {completion(nil); return }
-//        
-////        let url = unwrappedURL.appendingPathComponent(searchTerm)
-//        
-//        NetworkController.performRequest(for: url, httpMethod: .Get) { (data, error) in
-//            if let error = error {
-//                print(error.localizedDescription)
-//                completion(nil)
-//                return
-//            }
-//            guard let data = data else {completion(nil)
-//                return}
-//            
-//            guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String:Any] else { completion(nil)
-//                return}
-//        }
-////        _ = ["access_token":accessToken]
-//    }
-//}
+class UserController {
+    
+    static let baseURL = URL(string: "https://api.dribbble.com/v1/users/1/shots?")
+    
+    static func fetchUser(userID: Int, completion: @escaping([User]) -> Void) {
+        guard let url = baseURL else {return}
+        let parameters = ["count":"\(userID)"]
+        
+        NetworkController.performRequest(for: url, httpMethod: .Get, urlParameters: parameters, body: nil) { (data, error) in
+            
+            if let error = error {
+                print("Error with the request. Error: \(error.localizedDescription)")
+                completion([])
+            }
+            
+            guard let data = data,
+                let responseDataString = String(data: data, encoding: .utf8) else {
+                    print("")
+                    completion([])
+                    return}
+            
+            guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String: Any] else {completion([])
+                return}
+            
+            guard let userDictionaries = jsonDictionary["users"] as? [[String:Any]] else {completion([])
+                return}
+            
+            let users = userDictionaries.flatMap({User(dictionary: $0) })
+            completion(users)
+        }
+    }
+    
+}
+
+
+
