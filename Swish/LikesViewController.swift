0a73c4773e0e7b8e7705e902060858afe5c96f53
dmillerconsulting <dmillerconsulting@me.com>
2017-04-13 11:38:37 -0600

    ViewController and Cell Controller
    LikedShotsViewController set up
    LikedShotCell set up

--- Swish/LikesViewController.swift
+++ Swish/LikesViewController.swift
@@ -8,28 +8,62 @@
 
 import UIKit
 
-class LikesViewController: UIViewController {
+class LikesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: Properties
+    var shots: [Shot] = []
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var collectionView: UICollectionView!
 
+    // MARK: Lifecycle Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        fetchLikedShots()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: Delegat and Data Source Functions
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shots.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "shotCell", for: indexPath) as? LikedShotCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.shot = shots[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // send selected shot to ShotDetailViewController
+    }
+    
+    // API Call to get liked Shots
+    func fetchLikedShots() {
+        //https://api.dribbble.com/v1/user/likes?access_token=a1590f48ee53ae2d172f3c49a444ce3d658e92cf7c95a91cc39eebbd4c5197cd
+        
+        guard let likesBaseURL = URL(string: "https://api.dribbble.com/v1/user/likes") else { return }
+        let urlParameters = ["access_token": NetworkController.accessToken] as? [String:String]
+        
+        NetworkController.performRequest(for: likesBaseURL, httpMethod: .Get, urlParameters: urlParameters, body: nil) { (data, error) in
+            if error != nil {
+                NSLog("There was an error with the API to grab the user's liked shots: \(String(describing: error?.localizedDescription))")
+            }
+            
+            guard let data = data else { return }
+            
+            guard let likedShotsDictionariesArray = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [[String:Any]] else { return }
+            let likedShotsArray = likedShotsDictionariesArray.flatMap({ Shot(dictionary: $0) })
+            
+            self.shots = likedShotsArray
+            
+        }
+        
+    }
 
 }
