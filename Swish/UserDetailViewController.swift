2d2def04b18b392dfcfa2f7c375e06e0d659f226
clayjmills <claymills5@gmail.com>
2017-04-17 14:58:08 -0600

    Saving progress so I can pull new info from develop

--- Swish/UserDetailViewController.swift
+++ Swish/UserDetailViewController.swift
@@ -8,54 +8,51 @@
 
 import UIKit
 
-// numberOfItemsInSection
-// cellForItemAt
-// 
 
-//class UserDetailViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
-//
-//    var userAvatar = UIImage()
-//    var shots: [Shot] = []
-//    
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        UICollectionView.DataSource = self
-//        UICollectionView.Delegate = self
-//        
-//        // adjust to fetch all shots, not just liked shots
+class UserDetailViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+
+    var userAvatar = UIImage()
+    var shots: [Shot] = []
+    
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        collectionView.DataSource = self
+//        collectionView.Delegate = self
+        
+        // adjust to fetch all shots, not just liked shots
 //        ApiController.fetchLikedShots(page: String(page)) { (shots) in
 //            self.shots = shots
 //            DispatchQueue.main.async {
 //                self.collectionView.reloadData()
 //            }
 //        }
-//
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return shots.count
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "likedShotCell", for: indexPath) as? LikedShotCollectionViewCell else { return UICollectionViewCell() }
-//        
-//        cell.shot = shots[indexPath.row]
-//        return cell
-//    }
-//
-//
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destinationViewController.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shots.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "userShotCell", for: indexPath) as? LikedShotCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.shot = shots[indexPath.row]
+        return cell
+    }
+
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
