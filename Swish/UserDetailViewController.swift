6dea87db72cbe6f8976db7be1667f95946950a2d
clayjmills <claymills5@gmail.com>
2017-04-17 11:56:58 -0600

    Changed reuse identifyers and updated detail view controller

--- Swish/UserDetailViewController.swift
+++ Swish/UserDetailViewController.swift
@@ -8,29 +8,54 @@
 
 import UIKit
 
-class UserDetailViewController: UIViewController {
+// numberOfItemsInSection
+// cellForItemAt
+// 
 
-    var userAvatar = UIImage()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+//class UserDetailViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+//
+//    var userAvatar = UIImage()
+//    var shots: [Shot] = []
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        UICollectionView.DataSource = self
+//        UICollectionView.Delegate = self
+//        
+//        // adjust to fetch all shots, not just liked shots
+//        ApiController.fetchLikedShots(page: String(page)) { (shots) in
+//            self.shots = shots
+//            DispatchQueue.main.async {
+//                self.collectionView.reloadData()
+//            }
+//        }
+//
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return shots.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "likedShotCell", for: indexPath) as? LikedShotCollectionViewCell else { return UICollectionViewCell() }
+//        
+//        cell.shot = shots[indexPath.row]
+//        return cell
+//    }
+//
+//
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
