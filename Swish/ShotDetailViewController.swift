c1f1ae44b220f2adc4bae991e98db58f0fe6dc13
theyawninglion <theyawninglion@gmail.com>
2017-04-19 15:34:51 -0600

    switched the shotDetailSB from a viewController to a staticTableViewController and added all of the appropriate data to accomidate the change.

--- Swish/ShotDetailViewController.swift
+++ Swish/ShotDetailViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
+class ShotDetailViewController: UITableViewController, ShotRefreshDelegate {
+    
     
     //MARK: - outlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var creationDateLabel: UILabel!
     @IBOutlet weak var shotDescriptionTextView: UITextView!
-    @IBOutlet weak var userNameButton: UIButton!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var userAvatarImageView: UIImageView!
     @IBOutlet weak var likeCountLabel: UILabel!
@@ -24,42 +24,35 @@ class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
     @IBOutlet weak var likeLabel: UILabel!
     @IBOutlet weak var viewLabel: UILabel!
     
+    @IBOutlet weak var descriptionTableViewCell: UITableViewCell!
+    @IBOutlet weak var userTableViewCell: UITableViewCell!
+    
     //MARK: - actions
+    
     @IBAction func ShareButtonTapped(_ sender: Any) {
-        
         guard let shot = self.shotImageView.image else { return }
-        
         let activiityViewController = UIActivityViewController(activityItems: [shot], applicationActivities: nil)
-        
         present(activiityViewController, animated: true, completion: nil)
     }
     
     @IBAction func closeButtonTapped(_ sender: Any) {
-        
         dismiss(animated: true, completion: nil)
-        
     }
     
-    @IBAction func userNameButtonTapped(_ sender: Any) {
+    @IBAction func userButtonTapped(_ sender: Any) {
         
         let userStoryboard = UIStoryboard(name: "User", bundle: nil)
-        
         guard let userDetailVC = userStoryboard.instantiateViewController(withIdentifier: "userDetailVC") as? UserDetailViewController else { return }
-        
         userDetailVC.shotRefreshDelegate = self
-        
         self.present(userDetailVC, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = false
-        updateViews()
         views()
-        
     }
-    
-    
+
     var shot: Shot? {
         didSet {
             DispatchQueue.main.async {
@@ -76,12 +69,9 @@ class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
     func updateViews() {
         
         guard let shot = shot, let user = shot.user else { return }
-        
         let description = shot.description ?? ""
-        
         self.shotDescriptionTextView.text = description
         self.userNameLabel.text = "by \(user.userName) | \(user.userUserName)"
-        
         self.titleLabel.text = shot.title
         self.likeCountLabel.text = "\(shot.likeCount)"
         self.viewCountLabel.text = "\(shot.viewCount)"
@@ -90,7 +80,6 @@ class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
         if shot.largeImage != nil {
             self.shotImageView.image = shot.largeImage
         } else {
-            
             if shot.hiDpiImageURL == nil {
                 ImageController.image(forURL: shot.normalImageURL, completion: { (image) in
                     self.shotImageView.image = image
@@ -98,7 +87,9 @@ class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
             } else {
                 guard let hiDpiImageURL = shot.hiDpiImageURL else { return }
                 ImageController.image(forURL: hiDpiImageURL, completion: { (image) in
-                    self.shotImageView.image = image
+                    DispatchQueue.main.async {
+                        self.shotImageView.image = image
+                    }
                 })
             }
         }
@@ -114,6 +105,7 @@ class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
     }
     
     func views() {
+        
         guard let shot = shot else { return }
         shotImageView.image = shot.teaserImage
         
@@ -134,15 +126,41 @@ class ShotDetailViewController: UIViewController, ShotRefreshDelegate {
         likeCountLabel.text = ""
         creationDateLabel.text = ""
         
-        
-        
         //make the userAvatarImage round
         userAvatarImageView.layer.cornerRadius = userAvatarImageView.frame.size.width/2
         userAvatarImageView.clipsToBounds = true
         
+        //round the share button corners
         shareButton.layer.cornerRadius = 5
         shareButton.clipsToBounds = true
         
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var rowHeight : CGFloat = 0
+        if indexPath.row == 0 {
+            tableView.rowHeight = self.view.frame.height / 2.5
+            tableView.estimatedRowHeight = 281
+            rowHeight = tableView.rowHeight
+        }
+        if indexPath.row == 1 {
+            tableView.rowHeight = UITableViewAutomaticDimension
+            tableView.estimatedRowHeight = 281
+            rowHeight = tableView.rowHeight
+        }
+        if indexPath.row == 2 {
+            tableView.rowHeight = UITableViewAutomaticDimension
+            tableView.estimatedRowHeight = 281
+            rowHeight = tableView.rowHeight
+
+        }
+
+        if indexPath.row == 3 {
+            tableView.rowHeight = UITableViewAutomaticDimension
+            tableView.estimatedRowHeight = 281
+            rowHeight = tableView.rowHeight
+        }
+        return rowHeight
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
